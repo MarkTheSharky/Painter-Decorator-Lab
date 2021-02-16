7d319a1406592c472022cea117462f10a90aff86
nls20 <nls20@hotmail.co.uk>
2021-02-16 16:48:34 +0000

    initial commit

---
+++ specs/decorator_specs.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const Decorator = require('../decorator.js');
+
+describe('Decorator', function () {
+
+    let decorator;
+
+    beforeEach(function () {
+        decorator = new Decorator();
+    })
+})
